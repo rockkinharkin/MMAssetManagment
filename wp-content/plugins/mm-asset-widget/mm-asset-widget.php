5cd878eae7608ae733e407fccba8922bb9eebf97
Rachael Harkin <rachaelharkin2104@gmail.com>
2018-09-13 16:11:24 +0100

    course widget finally added for uploading assets

---
+++ wp-content/plugins/mm-asset-widget/mm-asset-widget.php
@@ -0,0 +1,237 @@
+<?php
+/*
+	Plugin Name: MM Asset Widget
+	Plugin URI: https://www.makematic.com/staff/assets-manager
+	Description: An widget to display resources for displayed asset
+	Version: 1.0
+	Author: Rachael Harkin
+	Author URI: http://www.hybrid.digital
+	Text Domain: mm-asset-widget
+	License: GPL3
+*/
+defined( 'ABSPATH' ) or die( 'Unauthorised Access' );
+
+// load scripts
+function mm_asset_widget_scripts() {
+   wp_enqueue_style( 'mmawidget-styles', plugins_url('css/mmawidget-styles.css',__FILE__ ) );
+   wp_enqueue_script( 'mmawidget-script', plugins_url('js/mmawidget.js',__FILE__ ), ['jquery'], '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'mm_asset_widget_scripts' );
+
+// Creating the widget
+require 'inc\aws-resources.php';
+
+class MM_Asset_Widget extends WP_Widget {
+
+  function __construct() {
+    $this->s3ResUrl   = 'http://courses.makematic.com/';
+    $this->audioSubDir  = '/audio/';
+    $this->imgSubDir  = '/images/';
+    $this->docsSubDir  = '/docs/';
+    $this->vidSubDir  = '/video/';
+
+  	$widget_ops = array(
+			'classname' => 'MM_Asset_Widget',
+			'description' => 'This widget displays the asset resources for the currently displayed asset',
+		);
+		parent::__construct( 'MM_Asset_Widget', 'MM Asset Widget', $widget_ops );
+  }
+
+  /**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+  public function widget( $args, $instance ) {
+    $content = "";
+    $queried_object = get_queried_object();
+    // setting defaults for $asset items
+    $wpasset = new stdClass();
+    $wpasset->ID = 0;
+    $wpasset->post_name= 'default-asset';
+    //$this->s3ResUrl.$wpasset->assetDir.$this->vidSubDir.[filename]
+
+    // getting current page widget is displayed on.
+    if ( $queried_object ) {
+        $wpasset = $queried_object;
+    }
+    // define location for remote asset directory.
+    $wpasset->assetDir = $wpasset->ID.'_'.$wpasset->post_name;
+
+    $title = apply_filters( 'widget_title', $instance['title'] );
+
+    if( is_user_logged_in() ){
+      //  need to check for licence here also.
+
+      // before and after widget arguments are defined by themes
+      echo $args['before_widget'];
+      if ( ! empty( $title ) )
+      echo $args['before_title'] . $title . $args['after_title'];
+
+      $content .= '<div class="mm-container">';
+      $content .= $this->buildVideoList($wpasset);
+      $content .= $this->buildAudioList($wpasset);
+      $content .= $this->buildImageList($wpasset);
+      $content .= $this->buildDocsList($wpasset);
+      $content .= '</div>';
+
+      echo __( $content, 'MM_Asset_widget' );
+      echo $args['after_widget'];
+    }
+  }
+
+  /**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+  public function form( $instance ) {
+    parent::form( $instance );
+
+    if ( isset( $instance[ 'title' ] ) ) {
+      $title = $instance[ 'title' ];
+    }
+    else {
+    $title = __( 'New title', 'MM_Asset_widget' );
+    }
+  // Widget admin form
+  ?>
+  <p>
+  <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+  <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+  </p>
+  <?php
+  }
+
+  /**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 *
+	 * @return array
+	 */
+  public function update( $new_instance, $old_instance ) {
+    $instance = array();
+    $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+    return $instance;
+  }
+
+//=======================================================================================================//
+//=========================================== HELPER FUNCTIONS ==========================================//
+
+  private function prepareS3FileObject($folder=NULL){
+    $s3Objects = new AWS_GetResources;
+    return $s3Objects->getResources($folder);
+  }
+
+  private function buildAudioList($wpasset=NULL){
+    $list = "";
+    $s3FileList = $this->prepareS3FileObject($wpasset->assetDir.$this->audioSubDir);
+
+    $list .=  '<div id="audio-list" class="mmarw-audio"><h4>Audio URLs</h4><ul>';
+    foreach ( $s3FileList as $a ){
+      $meta = $this->buildAssetMeta($a);
+      $list .=  '<li><a href="'.$this->s3ResUrl.$meta->full_path.'">'.$meta->display_fileName.'</a></li>';
+    }
+    $list .= '</ul></div>';
+    return $list;
+  }
+
+  private function buildImageList($wpasset=NULL){
+    $s3FileList = $this->prepareS3FileObject($wpasset->assetDir.$this->imgSubDir);
+    $list = "";
+    $list .= '<div id="image-list" class="mmarw-images"><h4>Images</h4><ul>';
+
+    foreach ( $s3FileList as $a ){
+      $meta = $this->buildAssetMeta($a);
+      if( strpos( $meta->file,'png') !== false ){ $class ='png'; }
+      if( ( strpos( $meta->file,'jpeg') !== false ) || ( strpos( $meta->file,'jpg') !== false) ){ $class ='jpg';}
+      if( strpos( $meta->file,'gif') !== false ){ $class ='gif';}
+      if( strpos( $meta->file,'tiff')!== false ){ $class ='tiff';}
+
+      $list .=  '<li class="'.$class.'"><a href="'.$this->s3ResUrl.$meta->full_path.'">'.$meta->display_fileName.'</a></li>';
+    }
+    $list .=  '</ul></div>';
+
+    return $list;
+  }
+
+  private function buildDocsList($wpasset=NULL){
+    $list=""; $class="doc";
+
+    $s3FileList = $this->prepareS3FileObject($wpasset->assetDir.$this->docsSubDir);
+    $list .= '<div id="docs-list" class="mmarw-docs"><h4>Documents</h4><ul>';
+
+    foreach ( $s3FileList as $a ){
+      $meta = $this->buildAssetMeta($a);
+
+      if( strpos( $meta->file,'docx')!== false ){ $class ='doc';}
+      if( strpos( $meta->file,'pdf') !== false ){ $class ='pdf';}
+      if( strpos( $meta->file,'odt')!== false ){ $class ='odt';}
+      if( strpos( $meta->file,'xls')!== false ){ $class ='xls';}
+      if( strpos( $meta->file,'txt')!== false ){ $class ='txt';}
+      if( strpos( $meta->file,'rtf')!== false ){ $class ='rtf';}
+
+      $list .=  '<li class="'.$class.'"><a href="'.$this->s3ResUrl.$meta->full_path.'">'.$meta->display_fileName.'</a></li>';
+    }
+    $list .=  '</ul></div>';
+    return $list;
+  }
+
+  private function buildVideoList($wpasset=NULL){
+    $list = "";
+    $s3FileList = $this->prepareS3FileObject( $wpasset->assetDir.$this->vidSubDir);
+    $list .= '<div id="embed-code" class="widget"><h4>Video Embed Code</h4>';
+
+    foreach ( $s3FileList as $a ){
+      $meta = $this->buildAssetMeta($a);
+
+
+      // if the 'Key' string contains asset->ID and 'video' and file types mp4,oog
+      if( ( strpos($meta->full_path, "mp4") !== false ) &&
+          ( $meta->asset_id == $wpasset->ID ) ){
+
+            if( strpos( $meta->file,'mp4') !== false ){ $type ='mp4'; }
+            if( strpos( $meta->file,'mp4') !== false ){ $type ='ogg'; }
+
+            $list .= '<h5>'.$meta->display_fileName.'</h5>
+                <textarea id="video-'.$wpasset->ID.'">
+                  <video width="320" height="240" controls>
+                  <source src="'.$meta->fullpath_without_file_extension.'" type="video/"'.$type.'>
+                  <source src="'.$meta->fullpath_without_file_extension.'.ogg" type="video/"'.$type.'>
+                  Your browser does not support the video tag.
+                  </video>
+                </textarea>';
+      }
+    }
+    $list .= '</div>';
+    return $list;
+  }
+
+  private function buildAssetMeta( $s3asset=NULL ){
+  //  print_r($s3asset);
+
+    $meta = new stdClass;
+    $meta->asset_id                        = 0;
+    $meta->coursename_slug                 = "";
+    $meta->full_path                       = $s3asset['Key'];
+    $meta->fullpath_without_file_extension = substr( $s3asset['Key'], 0, -4 ); // full filepath without file extension;
+
+    if( isset( $s3asset['Key'] ) &&  ( strpos( $s3asset['Key'],"_" ) !== false ) ){ // need this check to prevent "undefined offset: 1" error when the character doesnt exist in the string using the 'explode' function.
+
+      list( $meta->asset_id, $file )                          = explode( '_', $s3asset['Key'], 2 ); // array of strings [ assetid, filename,]
+      $fname                                                  = explode( '/', $file );
+      list( $assetId, $fileId,$meta->display_fileName,$date ) = explode( '_', $fname[2] );
+
+      $meta->file            = $fname[2];
+      $meta->coursename_slug = $fname[0];
+      $meta->nice_coursename = str_replace( '-',' ',$fname[0] );
+    }
+    return $meta;
+  }
+} // Class ends
+
+add_action( 'widgets_init', function(){	register_widget( 'MM_Asset_Widget' ); } );
+?>
---
+++ wp-content/plugins/mm-lifterlms-addons/mm-lifterlms-addons.php
@@ -0,0 +1,59 @@
+<?php
+/*
+!!! This plugin has a dependency for LifterLMS being installed.!!!
+
+	Plugin Name: MAKEMATIC Lifter LMS Extension pack
+	Plugin URI: https://www.makematic.com/lifterlms/extensionpack
+	Description: Bespoke features developed in conjucntion with LifterLMS for the MAKEMATIC asset system. Mainly
+  the linking & uploading of assets to a lifterlms course.
+	Version: 1.0
+	Author: Rachael Harkin
+	Author URI: http://hybrid.digital
+	Text Domain: asset-licence-manager
+	License: GPL3
+*/
+
+//defined( 'ABSPATH' ) || exit;
+//if (  is_plugin_active( ABSPATH.'wp-content/plugins/lifterlms/lifterlms.php' ) ) {
+
+
+// /**
+//  * Register meta box(es).
+//  */
+function wpdocs_register_meta_boxes() {
+    add_meta_box( 'mm-course-assets', __( 'Course Assets', 'mm-lifterlms-addons' ), 'build_course_assets_meta_box', 'course','side' );
+}
+add_action( 'add_meta_boxes', 'wpdocs_register_meta_boxes' );
+
+/* Meta Display callback
+ @param WP_Post $post Current post object.
+ */
+ function wpdocs_my_display_callback( $post ) {
+   echo "<div><h2>hello<h2></div>";
+    // Display code/markup goes here. Don't forget to include nonces!
+}
+
+function build_course_assets_meta_box( $post ){
+    wp_nonce_field( basename( __FILE__ ), 'mm_course_assets_meta_box_nonce' );
+
+    echo '<div id="course_builder" class="postbox ">
+                <button type="button" class="handlediv" aria-expanded="true"><span class="screen-reader-text">Toggle panel: Course Builder</span>
+                <span class="toggle-indicator" aria-hidden="true"></span>
+                </button>
+                      <div class="llms-builder-launcher">
+			                     <a class="llms-button-primary full" href="http://'.WP_SITEURL.'wp-admin/admin.php?page=llms-link-assets&amp;course_id="'.$post->ID.'>Attach Assets</a>
+	                     </div>
+		            </div>';
+  }
+  /**
+   * Save meta box content.
+   *
+   * @param int $post_id Post ID
+   */
+  function wpdocs_save_meta_box( $post_id ) {
+      // Save logic goes here. Don't forget to include nonce checks!
+  }
+
+  add_action( 'save_post', 'wpdocs_save_meta_box' );
+//}
+?>
