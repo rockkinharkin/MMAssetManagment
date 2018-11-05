46bd6c70ff8aeb96cf8cab228e1a647bce6e0a01
Rachael Harkin <rachaelharkin2104@gmail.com>
2018-11-05 16:24:11 +0000

    js functionality added to the asset widget for toggling resource areas

--- wp-content/plugins/mm-asset-widget/js/mmawidget-script.js
+++ wp-content/plugins/mm-asset-widget/js/mmawidget-script.js
@@ -3,25 +3,43 @@ jQuery(document).ready(function($){
 
   $('.mmarw-container').hide();
 
-  $('.item-class').on('click', function(){
-    $(this).each(function(ob){
-      $(ob+' h4').addClass('down-arrow');
-    });
-  });
-
   $('#video-list').on('click', function(){
-   $('#video-list .mmarw-container').toggle("slow");
+   $('#video-list .mmarw-container').toggle("slow", function(){
+     if( $(this).is(":visible")){
+      $('#video-list h4 span').replaceWith('<span class="list-open">-</span>');
+    }else{
+      $('#video-list h4 span').replaceWith('<span class="list-open">+</span>');
+    }
+   });
   });
 
   $('#audio-list').on('click', function(){
-   $('#audio-list .mmarw-container').toggle("slow");
+   $('#audio-list .mmarw-container').toggle("slow", function(){
+     if( $(this).is(":visible")){
+      $('#audio-list h4 span').replaceWith('<span class="list-open">-</span>');
+    }else{
+      $('#audio-list h4 span').replaceWith('<span class="list-open">+</span>');
+    }
+   });
   });
 
   $('#image-list').on('click', function(){
-   $('#image-list .mmarw-container').toggle("slow");
+   $('#image-list .mmarw-container').toggle("slow",function(){
+     if( $(this).is(":visible")){
+      $('#image-list h4 span').replaceWith('<span class="list-open">-</span>');
+    }else{
+      $('#image-list h4 span').replaceWith('<span class="list-open">+</span>');
+    }
+   });
   });
 
   $('#docs-list').on('click', function(){
-   $('#docs-list .mmarw-container').toggle("slow");
+   $('#docs-list .mmarw-container').toggle("slow",function(){
+     if( $(this).is(":visible")){
+      $('#docs-list h4 span').replaceWith('<span class="list-open">-</span>');
+     }else{
+      $('#docs-list h4 span').replaceWith('<span class="list-open">+</span>');
+     };
+   });
   });
 });
